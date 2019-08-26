f90c34614a59924c5b659477c339fb5eb10245a2
zion matthews <zman5682@gmail.com>
2019-08-26 11:23:50 -0500

    character update

---
+++ InventorySystem/Character.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    class Character
+    {
+        private string _name = "";
+        private int _xp = 0;
+        private int _level = 1;
+        private int[] _requiredXP = { 100, 300, 600, 1000 };
+
+
+
+        public Character(string name)
+        {
+            _name = name;
+        }
+
+        public string Name()
+        {
+            return _name;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_name);
+            Console.WriteLine("Level: " + _level);
+            Console.WriteLine("Experience: " + _xp);
+        }
+
+        public int Experience
+        {
+            get
+            {
+                return _xp;
+            }
+            set
+            {
+                _xp = value;
+                Console.WriteLine(_name + " gained experience and now has " + _xp);
+                if (_level <= _requiredXP.Length)
+                {
+
+
+                    if (_xp >= _requiredXP[_level - 1])
+                    {
+                        _level++;
+                        Console.WriteLine(_name + "'s level increased to " + _level + "!");
+                    }
+                }
+            }
+        }
+    }
+}
--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -10,8 +10,49 @@ namespace InventorySystem
     {
         static void Main(string[] args)
         {
-            Inventory inventory = new Inventory();
-            inventory.Menu();
+            //Inventory inventory = new Inventory();
+            //inventory.Menu();
+
+            Character player = new Character("Zion");
+            player.Print();
+
+            Character Leo = new Character("Leo");
+            Character Ryobi = new Character("Ryobi");
+            Leo.Print();
+            Ryobi.Print();
+
+            player.Experience = 30;
+            player.Experience = player.Experience + 50;
+            player.Experience++;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience += 100;
+            player.Experience += 100;
+            player.Experience += 100;
+            player.Experience += 100;
+            player.Experience += 100;
+            player.Experience += 100;
+            player.Experience += 100;
+            player.Experience += 100;
+            player.Experience += 100;
+            player.Experience += 100;
+
+            int[] testArry = new int[4];
+
+            testArry[0] = 1;
+            testArry[1] = 3;
+            testArry[2] = 5;
+            testArry[3] = 7;
+
+            int[] testArray2 = { 2, 3, 6, 8 };
+
+            string[] stringArry = new string[3];
+
+            Character[] party = { player, Leo, Ryobi, new Character("Like") };
+            
+
+
+            Console.ReadKey();
         }
     }
 }
